f938d1ff34d87cdfca93d84ae3460ed752ac0c3f
alessiopoli <150335994+jPoliX@users.noreply.github.com>
2024-10-01 11:22:45 +0200

    Create es1-iterazioni.c

---
+++ es1-iterazioni.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int N=0;
+    do{
+    printf("inserisci N: ");
+    scanf("%d",&N);
+    }while(N<0);
+     printf("numero successivo: %d\n", N+1);
+    return 0;
+}
